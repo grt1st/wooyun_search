339ee66e74c64d00c0117f2c9d20a7e2743c4b49
grt1st <jintianlu@qq.com>
2017-03-07 16:59:01 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+$config = array();
+$drops = array();
+
+$config['host'] = '127.0.0.1';
+$config['port'] = '3306';
+$config['user'] = 'root';
+$config['passwd'] = '';
+$config['database'] = 'wooyun';
+
+$drops['papers'] = '漏洞分析';
+$drops['tips'] = '技术分享';
+$drops['tools'] = '工具收藏';
+$drops['news'] = '业界资讯';
+$drops['web'] = 'web安全';
+$drops['pentesting'] = '渗透案例';
+$drops['mobile'] = '移动安全';
+$drops['wireless'] = '无线安全';
+$drops['database'] = '数据库安全';
+$drops['binary'] = '二进制安全';
+$drops['safe'] = '运维安全'
+
+?>
